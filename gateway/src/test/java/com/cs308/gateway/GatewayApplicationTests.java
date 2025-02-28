492690ddededb65b584ab1dd5a1b3706f8fb7bf3
Onur Orman <onur142ormn@gmail.com>
2025-02-28 23:14:14 +0300

    Created the basic configuration for the gateway microservice.

---
+++ gateway/src/test/java/com/cs308/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs308.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
