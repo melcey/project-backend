7fe208f2fa8cb8dcdffc05eb79599a673156d8ea
asaidildeniz <said.ildeniz@sabanciuniv.edu>
2025-03-08 11:49:16 +0300

    SCRUM-55 Add database infrastructure for sales manager registration - Created invoices table

---
+++ user-service/src/main/resources/db/migration/V8__Create_invoices_table.sql
@@ -0,0 +1,8 @@
+-- V8__Create_invoices_table.sql
+CREATE TABLE invoices (
+    invoice_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id),
+    invoice_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_price DECIMAL(10, 2) NOT NULL,
+    pdf_path TEXT
+);
