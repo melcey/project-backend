c98cde4c7b1f741cc62a2a6eb8f687e74847207a
Onur Orman <onur142ormn@gmail.com>
2025-03-08 21:29:48 +0300

    Successfully parameterized the configuration through the .env file.

--- README.md
+++ README.md
@@ -1,19 +1,42 @@
 # **project-backend**
 Backend code for the course project of the CS 308 course in Sabancı University.
 
-## **How to do remote debugging on IntelliJ?**
+## On the various details regarding configuration
+
+### **How to use .env to have your own configuration for passwords:**
+
+- Create a file named `.env`, and copy the content of .env.template into that file.
+
+- Fill the password fields to the .env file on your own; the rest will be handled within the project.
+
+- As the project progresses, new fields will be added to the .env file, so make sure you keep track of the developments in your configuration.
+
+- **Note: I am keeping the actual .env file on the repo for Sprint 1 alongside .env.template. From Sprint 2 onwards, I will update .gitignore to include the actual .env file, prompting you to generate your .env files on your own in the above specified way instead, so that you can secure your own personal configuration without affecting the rest of the repo. @OnurOrman**
+
+### **How to do remote debugging on IntelliJ?**
+
 - Click the current configuration name.
+
 - Click "Edit Configurations."
+
 - Click the "+" button, hovering "Add New Configuration."
+
 - In the dropdown menu, select "Remote JVM Debug" as the configuration type.
+
 - Keep the debugger mode as "Attach to remote JVM."
+
 - Enter the port specified in docker-compose.*.debug.yml files for the machine as the port.
-  - e.g., for `5006:5005`, enter **5006** as the port number, keeping the host as `localhost`.
-- Select name of the relevant module as the module classpath.
+
+  - e.g., for `5006:5005`, enter **`5006`** as the port number, keeping the host as `localhost`.
+
+- Select the name of the relevant module as the module classpath.
+
 - Save the configuration.
 
-## **How to do remote debugging on VS Code?**
+### **How to do remote debugging on VS Code?**
+
 - Following is an example configuration for VS Code to insert into the related `launch.json` file:
+
 ```json
 {
   "type": "java",
@@ -24,5 +47,7 @@ Backend code for the course project of the CS 308 course in Sabancı University.
   "projectName": "myapp"
 }
 ```
-- Here, you need to insert this snippet per Spring Boot project, and modify the fields according to the project you want to use.
+
+- Here, you need to insert this snippet into your `launch.json` file for each Spring Boot project, and modify the fields according to the project you want to use.
+
 - Then, go to the Run & Debug menu on the left sidebar, select your configuration from the dropdown menu, and run the configuration.
--- db_init.sql
+++ db_init.sql
@@ -1,44 +1,55 @@
 -- The script to initialize the prod and dev databases
 -- It only runs in the following conditions:
--- - When the database is going to be run for the first time
+-- - When the database is run for the first time
 -- - Or, when this script is updated
+
+-- Gets the environment variables from .env file
+\getenv postgres_db POSTGRES_DB
+\getenv userservice_role POSTGRES_USERSERVICE_ROLE
+\getenv userservice_user POSTGRES_USERSERVICE_USER
+\getenv userservice_password POSTGRES_USERSERVICE_PASSWORD
 
 -- Creates the project database if it does not exist
 -- Default schema, `public`, will be used, so we do not need a new schema
-SELECT 'CREATE DATABASE project_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'project_db')\gexec
+SELECT format('CREATE DATABASE %I', :'postgres_db')
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = :'postgres_db') \gexec
 
 -- Switches to the project database through the command invoked in psql
-\c project_db;
+\c :postgres_db
 
 -- Creates the roles for the database users per microservices
-DO $$
-BEGIN
-    -- Creates the user service role for that database user
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'user_service_role') THEN
-        CREATE ROLE user_service_role;
-    END IF;
+-- Creates the user service role if it doesn’t exist:
+SELECT 
+    CASE 
+      WHEN NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'userservice_role')
+      THEN format('CREATE ROLE %I', :'userservice_role')
+    END
+\gexec
 
-    -- Creates the database user for the user microservice
-    -- Note: Users and roles reside at the same namespace, pg_roles, in PostgreSQL
-        -- User is a role with the LOGIN privlege
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'user_service') THEN
-        -- The password is retrieved from the .env file and forcefully encrypted
-        CREATE USER user_service WITH ENCRYPTED PASSWORD 'user_db_pwd';
-    END IF;
-END $$;
+-- Creates the database user for the user microservice if it doesn’t exist:
+-- Note: Users and roles reside at the same namespace, pg_roles, in PostgreSQL
+    -- User is a role with the LOGIN privilege
+SELECT 
+    CASE 
+      WHEN NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'userservice_user')
+      THEN format('CREATE USER %I WITH ENCRYPTED PASSWORD %L', 
+                  :'userservice_user', 
+                  :'userservice_password')
+    END
+\gexec
 
--- Grants the following privileges to `user_service_role`:
--- user_service_role is able to connect to the database project_db
-GRANT CONNECT ON DATABASE project_db TO user_service_role;
--- user_service_role is able to access the database objects (tables, procedures, etc.) in the public schema of project_db
+-- Grants privileges to the user microservice role
+-- Allows the user microservice role to connect to the database
+SELECT format('GRANT CONNECT ON DATABASE %I TO %I', :'postgres_db', :'userservice_role') \gexec
+-- The user microservice role is able to access the database objects (tables, procedures, etc.) in the public schema of the project database
     -- Note: The individual privilege settings for the database objects will override this setting; this is just a global setting
-    -- user_service_role is also able to create new database objects in the public schema of project_db
-GRANT USAGE, CREATE ON SCHEMA public TO user_service_role;
+    -- The user microservice is also able to create new database objects in the public schema of the project database
+SELECT format('GRANT USAGE, CREATE ON SCHEMA public TO %I', :'userservice_role') \gexec
 
--- Grants the role to the user
-GRANT user_service_role TO user_service;
+-- Grants the user microservice role to the user microservice user
+SELECT format('GRANT %I TO %I', :'userservice_role', :'userservice_user') \gexec
 
+-- Creates the pgcrypto extension if it does not exist
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- Other privileges will be given on the basis of individual database objects to the roles in the related migrations
