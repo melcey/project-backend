400b67c896a35a3608e86fc1749673335542d58b
Onur Orman <onur142ormn@gmail.com>
2025-04-18 16:28:38 +0300

    A little clarification on the API URLs.

--- backend/src/main/java/com/cs308/backend/controller/CommentController.java
+++ backend/src/main/java/com/cs308/backend/controller/CommentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/api/comments")
+@RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
