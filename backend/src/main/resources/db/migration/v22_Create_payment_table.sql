bf41e737dc328e69a895e5c475f0bcb63d9cb089
asaidildeniz <said.ildeniz@sabanciuniv.edu>
2025-04-21 22:54:16 +0300

    SCRUM-37 Changed card_details to credit_card_id and made foreign attribution.

--- backend/src/main/resources/db/migration/v22_Create_payment_table.sql
+++ backend/src/main/resources/db/migration/v22_Create_payment_table.sql
@@ -4,12 +4,18 @@ CREATE TABLE payments (
     amount DECIMAL(10, 2) NOT NULL,
     payment_date TIMESTAMP NOT NULL,
     payment_status VARCHAR(20) NOT NULL CHECK (payment_status IN ('PENDING', 'COMPLETED', 'FAILED')),
-    card_details BYTEA,
+    credit_card_id INT,
     
     CONSTRAINT fk_payment_order
         FOREIGN KEY (order_id)
         REFERENCES orders (order_id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_payment_credit_card
+        FOREIGN KEY (credit_card_id)
+        REFERENCES credit_cards (credit_card_id)
+        ON DELETE SET NULL
 );
 
 CREATE INDEX idx_payment_order ON payments (order_id);
+CREATE INDEX idx_payment_credit_card ON payments (credit_card_id);
