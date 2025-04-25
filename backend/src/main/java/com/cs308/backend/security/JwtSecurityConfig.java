6731a59fb4f61294878a01e97c557f449f5b3e20
Onur Orman <onur142ormn@gmail.com>
2025-04-25 19:38:41 +0300

    Fixed the CORS configuration.

--- backend/src/main/java/com/cs308/backend/security/JwtSecurityConfig.java
+++ backend/src/main/java/com/cs308/backend/security/JwtSecurityConfig.java
@@ -102,7 +102,7 @@ public class JwtSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://frontend:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
